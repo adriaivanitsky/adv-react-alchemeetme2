9920ce708f559154d46f5a699a8bab2a8c6abf3a
Adria Ivanitsky <littleivanitsky@gmail.com>
2022-02-07 17:46:19 -0800

    replaced props with context in home.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,17 @@ import './App.css'
 import Layout from './views/Layout/Layout'
 import Home from './views/Home/Home'
 import fetchUser from './services/user'
+import { UserProvider } from './context/UserContext'
 
 function App() {
   // inital value should match the data type of end value
 
   return (
-    <Layout user={user}>
-      <Home user={user} />
-    </Layout>
+    <UserProvider>
+      <Layout>
+        <Home />
+      </Layout>
+    </UserProvider>
   )
 }
 
